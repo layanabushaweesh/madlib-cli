29b7fc4566e9901e2574b0aa258e9422160e28d6
layanabushaweesh <layanabushaweesh@gmail.com>
2021-06-12 21:46:58 +0300

    finsh

--- tests/mid_test.py
+++ tests/mid_test.py
@@ -1,12 +1,37 @@
-from madlib_cli.madlib_cli import read_template
 
-def test_read_template():
+from madlib_cli.madlib_cli import read_template, parse_template, merge
+
+import pytest
+
+def test_read_template_returns_stripped_string():
     actual = read_template("assets/oneline.txt")
     expected = "It was a {Adjective} and {Adjective} {Noun}."
     assert actual == expected
 
-from madlib_cli.madlib_cli import parse_template
-def test_parse():
-    actual = parse_template(read_template("assets/oneline.txt"),isTrue=False)
-    expected = ["Adjective", "Adjective", "Noun"]
+
+# @pytest.mark.skip("pending")
+def test_read_template_returns_stripped_string():
+    actual = read_template("assets/oneline.txt")
+    expected = "It was a {Adjective} and {Adjective} {Noun}."
     assert actual == expected
+
+
+def test_parse_template():
+    actual_parts = parse_template(read_template("assets/oneline.txt"),isTrue=False)
+    expected_parts = ["Adjective", "Adjective", "Noun"]
+    assert actual_parts == expected_parts
+
+
+def test_merge():
+    actual = merge(["dark", "stormy", "night"], parse_template(read_template("assets/oneline.txt"),isTrue=False) , read_template("assets/oneline.txt"))
+    expected = "It was a dark and stormy night."
+    assert actual == expected
+
+
+
+
+
+
+
+
+
